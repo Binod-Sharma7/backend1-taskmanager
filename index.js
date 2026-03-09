6f2ebb389663ed89507b5807134b39513588a1a5
Binod-Sharma7 <156831593+Binod-Sharma7@users.noreply.github.com>
2026-03-10 01:09:17 +0545

    changed ui , made attractive

--- index.js
+++ index.js
@@ -14,19 +14,21 @@ app.get('/', (req, res) => {
         
     });
 });
-app.get('/task/:filename', (req, res) => {
-    fs.readFile(`./files/${req.params.filename}.txt`, 'utf-8', (err, data) => {
+app.get('/task', (req, res) => {
+    const filename = req.query.filename || '';
+    fs.readFile(`./files/${filename}.txt`, 'utf-8', (err, data) => {
         if (err) {
             console.log(err);   
 
             res.status(500).send('Error reading task details');
         } else {
-            res.render('task', { title: req.params.filename, details: data });
+            res.render('task', { title: filename, details: data });
         }   
     });
 }); 
-app.post('/delete/:filename', (req, res) => {
-    fs.unlink(`./files/${req.params.filename}.txt`, (err) => {
+app.post('/delete', (req, res) => {
+    const filename = req.body.filename || '';
+    fs.unlink(`./files/${filename}.txt`, (err) => {
         if (err) {
             console.log(err);
             res.status(500).send('Error deleting task');
@@ -35,8 +37,33 @@ app.post('/delete/:filename', (req, res) => {
         }       
     });
 });
+app.get('/edit', (req, res) => {
+    const filename = req.query.filename || '';
+    res.render('edit', { title: filename });
+});
+app.post('/edit', (req, res) => {
+    const oldFilename = req.body.previous || '';
+    const newFilename = req.body.new ? req.body.new.split(' ').join('') : '';
+    fs.rename(`./files/${oldFilename}.txt`, `./files/${newFilename}.txt`, (err) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error renaming task');
+        }
+        else {
+            res.redirect('/');
+        }   
+    });
+});
+
 app.post('/create-task', (req, res) => {
-    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err) => {  
+    let filename = req.body.title ? req.body.title.split(' ').join('') : 'Untitled';
+    let counter = 1;
+    let finalFilename = filename;
+    while (fs.existsSync(`./files/${finalFilename}.txt`)) {
+        finalFilename = `${filename}(${counter})`;
+        counter++;
+    }
+    fs.writeFile(`./files/${finalFilename}.txt`, req.body.details, (err) => {  
         if (err) {
             console.log(err);
             res.status(500).send('Error creating task');
